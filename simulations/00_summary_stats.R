54179bb669fe61ff8b468c08a398090a17bdbc87
Rachael V. Phillips <rachaelvphillips@gmail.com>
2025-06-03 10:03:08 -0700

    organize sims folder

--- simulations/00_summary_stats.R
+++ simulations/00_summary_stats.R
@@ -13,20 +13,24 @@ summary_stats <- function(estimator_name, psi, se = NULL, psi_0 = 1.685959,
   if(is.null(var) & is.null(se)) {
     Coverage_analyticSE <- NA
     Ratio_BiasSE_analytic <- NA
+    avg_analyticSE <- NA
   } else if(!is.null(se)) {
     Ratio_BiasSE_analytic <- mean((psi - psi_0)/se)
     ci_lower <- psi - 1.96*se
     ci_upper <- psi + 1.96*se
     Coverage_analyticSE <- mean(ci_lower <= psi_0 & ci_upper >= psi_0)
+    avg_analyticSE <- mean(se)
   } else if(!is.null(var)) {
     Ratio_BiasSE_analytic <- mean((psi[i] - psi_0)/sqrt(var))
     ci_lower <- psi - 1.96*sqrt(var)
     ci_upper <- psi + 1.96*sqrt(var)
     Coverage_analyticSE <- mean(ci_lower <= psi_0 & ci_upper >= psi_0)
+    avg_analyticSE <- mean(sqrt(var))
   }
   
-  return_vec <- data.frame(Estimator = estimator_name, Bias = Bias, 
-                           SE_bootstrap = SE_bootstrap, MSE = MSE, 
+  return_vec <- data.frame(Estimator = estimator_name, Bias = Bias, MSE = MSE, 
+                           SE_bootstrap = SE_bootstrap, 
+                           avg_analyticSE = avg_analyticSE, 
                            Ratio_BiasSE_bootstrap = Ratio_BiasSE_bootstrap,
                            Ratio_BiasSE_analytic = Ratio_BiasSE_analytic,
                            Coverage_bootstrapSE = Coverage_bootstrapSE, 
--- simulations/01_run_simple_simulation.R
+++ simulations/01_run_simple_simulation.R
@@ -2,12 +2,9 @@ library(atmle)
 library(purrr)
 library(xgboost)
 library(tmle)
-library(foreach)
-library(doParallel)
-library(parallel)
 
-source("~/atmle-fusionRCT/sim_data.R")
-source("~/atmle-fusionRCT/summary_stats.R")
+source("~/atmle-fusionRCT/simulations/00_sim_data.R")
+source("~/atmle-fusionRCT/simulations/00_summary_stats.R")
 B <- 100
 n <- 5000
 p_S <- 0.2
@@ -18,12 +15,9 @@ psi0S_interaction <- -15
 RNGkind(sample.kind = "Rejection")
 set.seed(1517)
 seeds <- sample(1:2^15, B)
-save(seeds, file = "~/atmle-fusionRCT/output/seeds.Rdata")
+save(seeds, file = "~/atmle-fusionRCT/simulations/output/seeds.Rdata")
 
-registerDoParallel(cores = detectCores()-2)
-getDoParWorkers()
-result_list <- list()
-for(i in 61:B){
+for(i in 1:B){
   print(paste0("Starting simulation ", i))
 
   set.seed(seeds[i])
@@ -32,67 +26,45 @@ for(i in 61:B){
   ############### subgroup-specific data fusion effect estimates ###############
   start <- proc.time()
   set.seed(seeds[i])
-  atmle_res <- atmle(data = d,
-                     S = 1,
-                     W = c(2, 3),
-                     A = 4,
-                     Y = 5,
-                     controls_only = FALSE,
-                     family = "gaussian",
-                     theta_method = "sl3",
-                     Pi_method = "sl3",
-                     g_method = "sl3",
-                     Q_method = "sl3",
-                     theta_tilde_method = "sl3",
-                     bias_working_model = "HAL",
-                     pooled_working_model = "HAL",
-                     enumerate_basis_args = list(num_knots = 10),
-                     verbose = FALSE)
+  atmle_res <- atmle(
+    data = d, S = 1, W = c(2, 3), A = 4, Y = 5, controls_only = FALSE,
+    family = "gaussian", theta_method = "sl3", Pi_method = "sl3", g_method = "sl3",
+    Q_method = "sl3", theta_tilde_method = "sl3", bias_working_model = "HAL",
+    pooled_working_model = "HAL", enumerate_basis_args = list(num_knots = 10),
+    verbose = FALSE
+  )
   res_atmle <- data.frame(
-    psi = atmle_res$est, 
-    se = (atmle_res$upper-atmle_res$est)/1.96
+    psi = atmle_res$est, se = (atmle_res$upper-atmle_res$est)/1.96
   )
-  print(paste0("atmle_res for simulation ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
-
+  timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  print(paste0("atmle_res for simulation ", i, " time: ", timer))
+  
   start <- proc.time()
   set.seed(seeds[i])
-  atmle_res_glmnet <- atmle(data = d,
-                     S = 1,
-                     W = c(2, 3),
-                     A = 4,
-                     Y = 5,
-                     controls_only = FALSE,
-                     family = "gaussian",
-                     verbose = FALSE)
+  atmle_res_glmnet <- atmle(
+    data = d, S = 1, W = c(2, 3), A = 4, Y = 5, controls_only = FALSE,
+    family = "gaussian", verbose = FALSE
+  )
   res_atmle_glmnet <- data.frame(
-    psi = atmle_res_glmnet$est, 
-    se = (atmle_res_glmnet$upper-atmle_res_glmnet$est)/1.96
+    psi = atmle_res_glmnet$est, se = (atmle_res_glmnet$upper-atmle_res_glmnet$est)/1.96
   )
-  print(paste0("atmle_res_glmnet for simulation ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
-
+  timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  print(paste0("atmle_res_glmnet for simulation ", i, " time: ", timer))
+  
   start <- proc.time()
   set.seed(seeds[i])
-  atmle_res_hal <- atmle(data = d,
-                         S = 1,
-                         W = c(2, 3),
-                         A = 4,
-                         Y = 5,
-                         controls_only = FALSE,
-                         family = "gaussian",
-                         theta_method = "glm",
-                         Pi_method = "HAL",
-                         g_method = "glm",
-                         Q_method = "HAL",
-                         theta_tilde_method = "glm",
-                         bias_working_model = "HAL",
-                         pooled_working_model = "HAL",
-                         enumerate_basis_args = list(num_knots = 10),
-                         verbose = FALSE)
-  res_atmle_hal <- data.frame(
-    psi = atmle_res_hal$est, 
-    se = (atmle_res_hal$upper-atmle_res_hal$est)/1.96
+  atmle_res_hal <- atmle(
+    data = d, S = 1, W = c(2, 3), A = 4, Y = 5, controls_only = FALSE,
+    family = "gaussian", theta_method = "glm", Pi_method = "HAL", g_method = "glm",
+    Q_method = "HAL", theta_tilde_method = "glm", bias_working_model = "HAL", 
+    pooled_working_model = "HAL", enumerate_basis_args = list(num_knots = 10),
+    verbose = FALSE
   )
-  print(paste0("atmle_res_hal for simulation ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
+  res_atmle_hal <- data.frame(
+    psi = atmle_res_hal$est, se = (atmle_res_hal$upper-atmle_res_hal$est)/1.96
+  )
+  timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  print(paste0("atmle_res_hal for simulation ", i, " time: ", timer))
   
   #################### subgroup-specific effect estimates ######################
   d_S <- d[d$S == 1,]
@@ -105,33 +77,38 @@ for(i in 61:B){
     g1W = rep(0.5, nrow(d_S)), Q.SL.library = c("SL.glm"),
     family = "gaussian"
   )
-  res_tmle_glmQ <- data.frame(psi=tmle_res_glmQ$estimates$ATE$psi, 
-                              se=sqrt(tmle_res_glmQ$estimates$ATE$var.psi))
-  print(paste0("tmle_res_glmQ for sim ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
+  res_tmle_glmQ <- data.frame(
+    psi=tmle_res_glmQ$estimates$ATE$psi, se=sqrt(tmle_res_glmQ$estimates$ATE$var.psi)
+  )
+  timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  print(paste0("tmle_res_glmQ for sim ", i, " time: ", timer))
   
   # TMLE ATE estimator w SL estimation for Q
-  # start <- proc.time()
   # set.seed(seeds[i])
   # tmle_res_slQ <- tmle(
   #   Y = d_S$Y, A = d_S$A, W = d_S[, c("W1", "W2")], 
-  #   g1W = tmle_res_glmQ$g$g1W,
+  #   g1W = rep(0.5, nrow(d_S)),
   #   family = "gaussian"
   # )
-  # res_tmle_slQ <- data.frame(psi=tmle_res_slQ$estimates$ATE$psi, 
-  #                            se=sqrt(tmle_res_slQ$estimates$ATE$var.psi))
-  # print(paste0("tmle_res_slQ for sim ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
-  # 
+  # res_tmle_slQ <- data.frame(
+  #   psi=tmle_res_slQ$estimates$ATE$psi, se=sqrt(tmle_res_slQ$estimates$ATE$var.psi)
+  # )
+  # timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  # print(paste0("tmle_res_slQ for sim ", i, " time: ", timer))
+  
   # TMLE ATE estimator w GLM estimation for Q + GLM estimation for g
   start <- proc.time()
   set.seed(seeds[i])
   tmle_res_glmQ_glmG <- tmle(
     Y = d_S$Y, A = d_S$A, W = d_S[, c("W1", "W2")], 
-    g.SL.library = c("SL.glm"), Q = tmle_res_glmQ$Qinit$Q,
+    g1W = c("SL.glm"), Q = tmle_res_glmQ$Qinit$Q,
     family = "gaussian"
   )
-  res_tmle_glmQ_glmG <- data.frame(psi=tmle_res_glmQ_glmG$estimates$ATE$psi, 
-                              se=sqrt(tmle_res_glmQ_glmG$estimates$ATE$var.psi))
-  print(paste0("tmle_res_slQ_glmG for sim ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
+  res_tmle_glmQ_glmG <- data.frame(
+    psi=tmle_res_glmQ_glmG$estimates$ATE$psi, se=sqrt(tmle_res_glmQ_glmG$estimates$ATE$var.psi)
+  )
+  timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  print(paste0("tmle_res_slQ_glmG for sim ", i, " time: ", timer))
   
   # TMLE ATE estimator w SL estimation for Q + GLM estimation for g
   # start <- proc.time()
@@ -141,9 +118,11 @@ for(i in 61:B){
   #   g1W = tmle_res_glmQ_glmG$g$g1W, Q = tmle_res_slQ$Qinit$Q,
   #   family = "gaussian"
   # )
-  # res_tmle_slQ_glmG <- data.frame(psi=tmle_res_slQ_glmG$estimates$ATE$psi, 
-  #                            se=sqrt(tmle_res_slQ_glmG$estimates$ATE$var.psi))
-  # print(paste0("tmle_res_glmQ_glmG for sim ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
+  # res_tmle_slQ_glmG <- data.frame(
+  #   psi=tmle_res_slQ_glmG$estimates$ATE$psi, se=sqrt(tmle_res_slQ_glmG$estimates$ATE$var.psi)
+  # )
+  # timer <- as.numeric((proc.time()-start)["elapsed"])/60
+  # print(paste0("tmle_res_slQ for sim ", i, " time: ", timer))
   
   # unadjusted ATE estimator
   mod_unadj <- lm(Y ~ A, data = d_S)
@@ -160,7 +139,7 @@ for(i in 61:B){
     res_unadj = res_unadj
   )
   start <- proc.time()
-  save(res_list, file = paste0("~/atmle-fusionRCT/output/res",i,".Rdata"))
+  save(res_list, file = paste0("~/atmle-fusionRCT/simulations/output/res_simple",i,".Rdata"))
   print(paste0("saving sim ", i, " time: ", as.numeric((proc.time()-start)["elapsed"])/60))
   
   result_list[[i]] <- res_list
@@ -169,7 +148,7 @@ for(i in 61:B){
 result_list <- list()
 for (i in 1:B) {
   print(i)
-  load(paste0("~/atmle-fusionRCT/output/res",i,".Rdata"))
+  load(paste0("~/atmle-fusionRCT/simulations/output/res_simple",i,".Rdata"))
   result_list[[i]] <- res_list
 }
 
@@ -204,4 +183,4 @@ res <- data.frame(
         tmle_slQ_stats, tmle_glmQ_glmG_stats, 
         tmle_slQ_glmG_stats, unadj_stats)
 )
-write.csv(res, file = "~/atmle-fusionRCT/results_simple.csv", row.names = F)
+write.csv(res, file = "~/atmle-fusionRCT/simulations/results_simple.csv", row.names = F)
